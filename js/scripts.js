a1b4c6019590107460f55482de8fbacf15e42180
John Klaras & Matt Carlson <student@epicodus.com>
2015-09-30 20:54:22 -0700

    add a bunch of stuff.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 $(function() {
-  $("p").click(function() {
-    $("img").show();
+  $(".clickable").click(function() {
+    $("#initially-hidden").slideToggle();
+    $("#initially-showing").slideToggle();
   });
+  $(".hovercraft").dblclick(function() {
+    $("#no-penguin").fadeToggle();
+    $("#yes-penguin").fadeToggle();
+  });
+  $("#polar").dblclick(function() {
+    $(".image").fadeIn();
+    
+  });
+
 });
